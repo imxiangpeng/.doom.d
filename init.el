db99149c176bf78c0f43e024e6b838be21426458
Alex Meng <mengxp@inspur.com>
2021-05-27 10:37:11 +0800

    do not use dragndrop, because it always insert attachment
    Signed-off-by: Alex Meng <mengxp@inspur.com>

--- init.el
+++ init.el
@@ -76,7 +76,7 @@
        ;;eshell            ; the elisp shell that works everywhere
        ;;shell             ; simple shell REPL for Emacs
        ;;term              ; basic terminal emulator for Emacs
-       ;;vterm             ; the best terminal emulation in Emacs
+       vterm             ; the best terminal emulation in Emacs
 
        :checkers
        syntax              ; tasing you for every semicolon you forget
@@ -150,8 +150,7 @@
          +roam
          +pandoc
          +pretty
-         +present
-         +dragndrop)               ; organize your plain life in plain text
+         +present)               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
        ;;purescript        ; javascript, but functional
